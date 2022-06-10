ed151060b80e7d006fe248b3943b38ffce7f0336
Tiffany Rose <tiffany.drose@gmail.com>
2022-06-10 13:53:10 -0400

    finished scaffolding the html, working on javascript

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# to-do-list
+# ToDo List App Project
+
+This app was built using bootstrap, html, css, and javascript
+
+##Features
+
+```add and remove buttons
+```text input for creating new task
+
---
+++ script.js
@@ -0,0 +1,49 @@
+console.log("hello world");
+
+// Data Structure
+const arrayOfTasks = [
+  {
+    task: "walk the dog",
+    checked: false,
+  },
+  {
+    task: "walk the dog",
+    checked: false,
+  },
+  {
+    task: "walk the dog",
+    checked: false,
+  },
+  {
+    task: "walk the dog",
+    checked: false,
+  },
+];
+
+// JS Variables
+
+// DOM Variables
+const newTaskInput = document.getElementById("new-task");
+const addBtn = document.getElementById("add-task");
+const taskList = document.querySelector(".content");
+// Function Defintions
+
+// makeInputGroup()
+function makeInputGroup(input) {
+    const newDivPrepend = document.createElement("div");
+    const newInput = document.createElement("input");
+}
+
+//createTask()
+function createTask() {
+  for (let i = 0; i < arrayOfTasks.length; i++) {
+
+
+  }
+}
+// removeTask()
+
+// Event Listeners
+
+// addButton --> click,createTask()
+// removeButton --> click, removeTask()
