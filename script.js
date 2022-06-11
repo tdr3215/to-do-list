54bb3bc8875412c52b730bc4948524b288e54868
Tiffany Rose <tiffany.drose@gmail.com>
2022-06-11 00:51:06 -0400

    finished createTask Function

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ const arrayOfTasks = [
     checked: false,
   },
   {
-    task: "walk the dog",
+    task: "bake the chicken",
     checked: false,
   },
   {
-    task: "walk the dog",
+    task: "pick up child",
     checked: false,
   },
   {
-    task: "walk the dog",
+    task: "put down milk",
     checked: false,
   },
 ];
@@ -28,19 +28,42 @@ const addBtn = document.getElementById("add-task");
 const taskList = document.querySelector(".content");
 // Function Defintions
 
-// makeInputGroup()
-function makeInputGroup(input) {
-    const newDivPrepend = document.createElement("div");
-    const newInput = document.createElement("input");
-}
-
 //createTask()
 function createTask() {
   for (let i = 0; i < arrayOfTasks.length; i++) {
+    const task = arrayOfTasks[i];
 
+    const newTask = task.task;
 
+    const inputGroupDiv = document.createElement("div");
+    const prependDiv = document.createElement("div");
+    const inputTextDiv = document.createElement("div");
+    const checkBox = document.createElement("input");
+    const readOnlyText = document.createElement("div");
+    const appendDiv = document.createElement("div");
+    const removeButton = document.createElement("button");
+
+    inputGroupDiv.className = "input-group mb-3";
+    prependDiv.className = "input-group-prepend";
+    inputTextDiv.className = "input-group-text";
+    checkBox.type = "checkbox";
+    readOnlyText.className = "card-body";
+    readOnlyText.innerText = newTask;
+    appendDiv.className = "input-group-append";
+    removeButton.className = "btn btn-danger";
+    removeButton.innerText = "Remove";
+
+    taskList.append(inputGroupDiv);
+    inputGroupDiv.append(prependDiv);
+    inputGroupDiv.append(readOnlyText);
+    inputGroupDiv.append(appendDiv);
+    prependDiv.append(inputTextDiv);
+    inputTextDiv.append(checkBox);
+    appendDiv.append(removeButton);
   }
 }
+
+createTask();
 // removeTask()
 
 // Event Listeners
